81c242ec27b8c9e285527a5e987d7ab55ec99999
Ashley Scillitoe <ashleyscillitoe@googlemail.com>
2021-06-25 15:06:51 +0100

    Started conversion of tutorials to notebooks

--- source/conf.py
+++ source/conf.py
@@ -1,9 +1,11 @@
 import os
 import sys
+
+# Misc Sphinx settings
+#########################
 project = u' '
 copyright = u'2016-2021 by equadratures.org'
 author = u'equadratures'
-# -- Bryn: Main Vuepress website location ------------------------------------
 land_page = "equadratures.org"
 outdir = sys.argv[-1]   #Bryn: Assuming last system argument is output directory
 version = u'9.1'
@@ -20,9 +22,9 @@ extensions = [
     'sphinx.ext.githubpages',
     'sphinxcontrib.napoleon',
     'sphinx_panels',
-]
-templates_path = ['_templates']
-source_suffix = '.txt'
+    'sphinx_copybutton',
+    'nbsphinx',
+] #TODO - prune unused extensions
 master_doc = 'index'
 html_title = 'equadratures'
 html_theme = 'pydata_sphinx_theme'
@@ -30,18 +32,15 @@ html_logo = 'logo_new.png'
 html_favicon = 'eq-logo-favicon.png'
 language = None
 exclude_patterns = [u'_docsbuild', 'Thumbs.db', '.DS_Store']
-html_static_path = ['_static']
-html_css_files = [
-        'css/styles.css',
-        'css/footer.css',
-        'css/cards.css'
-        ]
-panels_add_bootstrap_css = False # sphinx-panels shouldn't add bootstrap css since the pydata-sphinx-theme already loads it
+source_suffix = '.txt'
 htmlhelp_basename = 'EffectiveQuadraturesdoc'
 latex_elements = {
     'papersize': 'a4paper',
     'pointsize': '12pt',
 }
+
+# Manual and text info outputs
+#########################
 man_pages = [
     (master_doc, 'effectivequadratures', u'Effective Quadratures Documentation',
      [author], 1)
@@ -51,6 +50,57 @@ texinfo_documents = [
      author, 'equadratures', 'equadratures is an open-source python code developed by Effective Quadratures. It is tailored for tackling problems in uncertainty quantification, surrogate-based optimisation, numerical integration, and data-driven dimension reduction. ',
      'Miscellaneous'),
 ]
+
+# Templates, static files, and css (bug with css being copied to _static/css, this must be done manually for now)  
+#########################
+templates_path = ['_templates']
+html_static_path = ['_static']
+html_css_files = [
+        'css/styles.css',
+        'css/footer.css',
+        'css/cards.css'
+        ]
+
+# Sphinx panels
+#########################
+panels_add_bootstrap_css = False # sphinx-panels shouldn't add bootstrap css since the pydata-sphinx-theme already loads it
+
+# copybutton settings
+#########################
+copybutton_prompt_text = r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: "
+copybutton_prompt_is_regexp = True
+copybutton_line_continuation_character = "\\"
+
+# nbsphinx setting
+#########################
+nbsphinx_execute_arguments = [
+    "--InlineBackend.figure_formats={'svg', 'pdf'}",
+    "--InlineBackend.rc=figure.dpi=96",
+]
+nbsphinx_input_prompt = 'In [%s]:'
+nbsphinx_output_prompt = 'Out[%s]:'
+# This is processed by Jinja2 and inserted before each notebook
+nbsphinx_prolog = r""" 
+{% set docname = env.doc2path(env.docname, base=None) | replace('_documentation/', '') %}
+
+.. raw:: html
+
+    <div class="admonition tutorial">
+      This page was generated from
+      <a class="reference external" href="https://github.com/equadratures/equadratures.github.io/blob/master/_documentation/{{ docname|e }}">{{ docname|e }}</a>.
+      <br>
+      Interactive online version:
+      <span style="white-space: nowrap;"><a href="https://colab.research.google.com/github/equadratures/equadratures.github.io/blob/master/_documentation/{{ docname|e }}"><img alt="Colab badge" src="https://colab.research.google.com/assets/colab-badge.svg" style="vertical-align:text-bottom"></a></span>
+    </div>
+
+.. raw:: latex
+
+    \nbsphinxstartnotebook{\scriptsize\noindent\strut
+    \textcolor{gray}{The following section was generated from
+    \sphinxcode{\sphinxupquote{\strut {{ docname | escape_latex }}}} \dotfill}}
+"""
+
+# Intersphinx mapping
 intersphinx_mapping = {
         'https://docs.python.org/': None,
         'numpy': ('https://numpy.org/doc/stable/', None),
@@ -59,6 +109,9 @@ intersphinx_mapping = {
         'sklearn': ('http://scikit-learn.org/stable',
             (None, './_intersphinx/sklearn-objects.inv'))
         }
+
+# pydata theme options
+#########################
 html_theme_options = {
     "external_links": [{"url": "https://discourse.equadratures.org/", "name": "Discourse"}],
 #    "github_url": "https://github.com/Effective-Quadratures/equadratures",
