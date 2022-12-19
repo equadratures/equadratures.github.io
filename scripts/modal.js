32a3a5cfe8f97aefc0ac6c10610390cae53d3f6d
Bryn N. Ubald <55503826+bnubald@users.noreply.github.com>
2022-12-19 23:58:33 +0000

    Tidy up

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,7 +1,7 @@
-function replacePlotlyIframe(item2) {
+function replacePlotlyIframe(item) {
     requirejs(["jquery"], function () {
         var i, frames;
-        frames = item2.getElementsByTagName("iframe");
+        frames = item.getElementsByTagName("iframe");
         var framepath;
         for (i = 0; i < frames.length; ++i) {
             var url = new URL(frames[i].title);
@@ -11,49 +11,23 @@ function replacePlotlyIframe(item2) {
     });
 }
 
-
 var goToModal = function (value) {
     requirejs(["jquery"], function () {
         var embedfile;
         var modal = $(value);
         embedfile = modal.attr("html_file");
 
-        // $('#replaceDiv').slideUp(0);
-
-        // var item = document.getElementById('mainDiv');
-        // item.classList.toggle("container");
-        // item.classList.toggle("infocontainer");
-        var item2 = document.getElementById('mainDiv');
-
-        // $('#mainDiv').slideUp(1000, function () {
-        //     // $('#replaceDiv').load(embedfile).ready(function () {
-        //     //     $('#replaceDiv').slideDown(2000, "linear");
-        //     // });
-        //     $('#replaceDiv').load(embedfile, async function () {
-        //         var i, frames;
-        //         frames = item2.getElementsByTagName("iframe");
-        //         for (i = 0; i < frames.length; ++i) {
-        //             frames[i].title = frames[i].src;
-        //             frames[i].src = "";
-        //         }
-        //         $(this).slideDown(2000, "linear", function () {
-        //             replacePlotlyIframe(item2);
-
-        //             // window.PlotlyConfig = { MathJaxConfig: 'local' }; if (window.MathJax && window.MathJax.Hub && window.MathJax.Hub.Config) { window.MathJax.Hub.Config({ SVG: { font: "STIX-Web" } }) }; if (typeof require !== 'undefined') { require.undef("plotly"); requirejs.config({ paths: { 'plotly': ['/scripts/vendor/plotly-gl3d-2.12.1.min'] } }); require(['plotly'], function (a) { window._Plotly = a }) }
-        //         });
-        //     });
-        // });
+        var mainDiv = document.getElementById('mainDiv');
 
         $('#mainDiv').load(embedfile, async function () {
             var i, frames;
-            frames = item2.getElementsByTagName("iframe");
+            frames = mainDiv.getElementsByTagName("iframe");
             for (i = 0; i < frames.length; ++i) {
                 frames[i].title = frames[i].src;
                 frames[i].src = "";
             }
-            replacePlotlyIframe(item2);
+            replacePlotlyIframe(mainDiv);
         });
-
 
     });
 };
