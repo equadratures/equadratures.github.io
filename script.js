1e7b31bd875165b2cf46e8ae690b083971959597
Bryn N. Ubald <55503826+bnubald@users.noreply.github.com>
2023-07-07 14:53:57 +0100

    Move header+footer to separate, use jscript to load

--- script.js
+++ script.js
@@ -1,3 +1,119 @@
+/**
+ * Animation on scroll
+ */
+AOS.init({
+  easing: "ease-out-back",
+  duration: 800,
+  delay: 0,
+  once: true,
+  anchorPlacement: "bottom-center",
+  //   disable: "mobile",
+});
+
+// Function to load the header
+function loadHeader() {
+  var headerContainer = document.getElementById("header");
+  var headerRequest = new XMLHttpRequest();
+
+  headerRequest.open("GET", "header.html", true);
+  headerRequest.onreadystatechange = function () {
+    if (headerRequest.readyState === 4 && headerRequest.status === 200) {
+      headerContainer.innerHTML = headerRequest.responseText;
+      markActivePage();
+    }
+  };
+
+  headerRequest.send();
+}
+
+// Function to load the footer
+function loadFooter() {
+  var footerContainer = document.getElementById("footer");
+  var footerRequest = new XMLHttpRequest();
+
+  footerRequest.open("GET", "footer.html", true);
+  footerRequest.onreadystatechange = function () {
+    if (footerRequest.readyState === 4 && footerRequest.status === 200) {
+      footerContainer.innerHTML = footerRequest.responseText;
+    }
+  };
+
+  footerRequest.send();
+}
+
+// Function to mark the active page in the navigation
+function markActivePage() {
+  var navigationItems = document.querySelectorAll("#header nav a");
+  var currentPage = window.location.pathname;
+
+  var spanElement = document.createElement("span");
+  spanElement.setAttribute("class", "sr-only");
+  spanElement.textContent = "(current)";
+
+  for (var i = 0; i < navigationItems.length; i++) {
+    var link = navigationItems[i].getAttribute("href");
+
+    if (currentPage.includes(link)) {
+      // Add screen reader target.
+      navigationItems[i].appendChild(spanElement);
+      // Make element active.
+      navigationItems[i].classList.add("active");
+    }
+  }
+  // If on equadratures.org/ without 'index.html', set "Home" to be active
+  if (currentPage == "/") {
+    navigationItems[0].classList.add("active");
+  }
+}
+
+// // Function to load the page content
+// function loadPageContent(pageUrl) {
+//   var mainContainer = document.getElementById("main");
+//   var request = new XMLHttpRequest();
+
+//   request.open("GET", pageUrl, true);
+//   request.onreadystatechange = function () {
+//     if (request.readyState === 4 && request.status === 200) {
+//       mainContainer.innerHTML = request.responseText;
+//       markActivePage(); // Update the active page after loading the content
+//     }
+//   };
+
+//   request.send();
+// }
+
+// Function to load file content using jquery.
+function loadFileContents(filePath, setPath = null) {
+  // alert(filePath);
+  // main = document.getElementById("main");
+  // main.src = "index_content.html";
+  $("#main").load(filePath, function () {});
+  // $("#main").load(filePath);
+  if (setPath != null) {
+    window.history.pushState(null, "", setPath);
+  }
+}
+
+// Function to load the file contents.
+function loadFileContents(filePath, setPath = null) {
+  var mainContainer = document.getElementById("main");
+  var mainRequest = new XMLHttpRequest();
+
+  mainRequest.open("GET", filePath, true);
+  mainRequest.onreadystatechange = function () {
+    if (mainRequest.readyState === 4 && mainRequest.status === 200) {
+      mainContainer.innerHTML = mainRequest.responseText;
+      markActivePage();
+    }
+  };
+
+  if (setPath != null) {
+    window.history.pushState(null, "", setPath);
+  }
+
+  mainRequest.send();
+}
+
 // Script to play/pause about equadratures video in index page
 // if atleast 30% of the video is visible on viewport.
 
--- scripts/modal.js
+++ scripts/modal.js
@@ -1,33 +1,79 @@
 function replacePlotlyIframe(item) {
-    requirejs(["jquery"], function () {
-        var i, frames;
-        frames = item.getElementsByTagName("iframe");
-        var framepath;
-        for (i = 0; i < frames.length; ++i) {
-            var url = new URL(frames[i].title);
-            framepath = url.pathname;
-            frames[i].src = "/modals/notebooks/" + framepath;
-        }
-    });
+  requirejs(["jquery"], function () {
+    var i, frames;
+    frames = item.getElementsByTagName("iframe");
+    var framepath;
+    for (i = 0; i < frames.length; ++i) {
+      var url = new URL(frames[i].title);
+      framepath = url.pathname;
+      frames[i].src = "/modals/notebooks/" + framepath;
+    }
+  });
 }
 
+const slideFadeOut = (elem) => {
+  const fade = { opacity: 0, transition: "opacity 400ms" };
+  elem.css(fade).slideUp();
+};
+
+const slideFadeIn = (elem) => {
+  const fade = { opacity: 1, transition: "opacity 400ms" };
+  elem.css(fade).slideUp();
+};
+
 var goToModal = function (value) {
-    requirejs(["jquery"], function () {
-        var embedfile;
-        var modal = $(value);
-        embedfile = modal.attr("html_file");
+  requirejs(["jquery"], function () {
+    var embedfile;
+    var modal = $(value);
+    // $("#mainDiv").attr("data-aos", "fade-up");
+    embedfile = modal.attr("html_file");
 
-        var mainDiv = document.getElementById('mainDiv');
+    var jupyterNotebook = document.getElementById("jupyterNotebook");
 
-        $('#mainDiv').load(embedfile, async function () {
-            var i, frames;
-            frames = mainDiv.getElementsByTagName("iframe");
-            for (i = 0; i < frames.length; ++i) {
-                frames[i].title = frames[i].src;
-                frames[i].src = "";
-            }
-            replacePlotlyIframe(mainDiv);
-        });
+    // $("#mainDiv").prepend('<div id="jupyterNotebook">...</div>');
+    // $("#mainDiv").before("<div></div>");
+    // $("#jupyterNotebook").attr("data-aos", "fade-up");
 
+    $("#jupyterNotebook").hide();
+
+    $("#jupyterNotebook").addClass("d-flex");
+    $("#jupyterNotebook").addClass("flex-column");
+    $("#jupyterNotebook").addClass("mx-5");
+    $("#jupyterNotebook").addClass("px-5 pb-5");
+    // $("#jupyterNotebook").css("margin-top", "62px");
+
+    $("#jupyterNotebook").load(embedfile, async function () {
+      var i, frames;
+      frames = jupyterNotebook.getElementsByTagName("iframe");
+      for (i = 0; i < frames.length; ++i) {
+        frames[i].title = frames[i].src;
+        frames[i].src = "";
+      }
+      replacePlotlyIframe(jupyterNotebook);
     });
+
+    // $("#mainDiv").attr("data-aos", "fade-up");
+    // $("#introContent").fadeOut();
+    // $("#jupyterNotebook").fadeIn();
+
+    $("#jupyterNotebook").ready(function () {
+      slideFadeOut($("#introContent"));
+      slideFadeIn($("#jupyterNotebook"));
+    });
+
+    // slideFadeOut($("#introContent"));
+    // slideFadeIn($("#jupyterNotebook"));
+
+    // var mainDiv = document.getElementById("mainDiv");
+
+    // $("#mainDiv").load(embedfile, async function () {
+    //   var i, frames;
+    //   frames = mainDiv.getElementsByTagName("iframe");
+    //   for (i = 0; i < frames.length; ++i) {
+    //     frames[i].title = frames[i].src;
+    //     frames[i].src = "";
+    //   }
+    //   replacePlotlyIframe(mainDiv);
+    // });
+  });
 };
