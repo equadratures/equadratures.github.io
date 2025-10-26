84fe9c158cc10da219204f0377344f7206754970
Bryn Noel Ubald <55503826+bnubald@users.noreply.github.com>
2025-10-26 02:28:36 +0000

    fix: Add site URL to config

--- astro.config.ts
+++ astro.config.ts
@@ -23,6 +23,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
+  site: 'https://equadratures.org/',
   output: 'static',
 
   integrations: [
